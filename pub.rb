fcc0b5c62671cfa851cd1b4c17fec32b6e37a1bd
Uzzal Devkota <theoozzal@gmail.com>
2014-08-11 11:16:57 +0545

    Publish user joined message.

--- pub.rb
+++ pub.rb
@@ -9,6 +9,8 @@ $redis = Redis.new
 
 data = {"user" => ARGV[1]}
 
+$redis.publish ARGV[0], data.merge('msg' => "#{ARGV[1]} has joined").to_json
+
 loop do
   msg = STDIN.gets
   $redis.publish ARGV[0], data.merge('msg' => msg.strip).to_json
